31156bffc49c7bf58ecf6cca9fa5bb0900ef4e95
Wayne Parrott <5588978+wayneparrott@users.noreply.github.com>
2019-03-14 15:46:41 -0500

    initial checkin

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component2.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  public value$: Observable<number>;
+
+  ngOnInit() {
+    this.value$ = interval(1000).pipe(
+      map(() => Math.random() * 120)
+    );
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GaugesModule } from 'ng-beautiful-gauges';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    GaugesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
